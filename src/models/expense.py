663c7901584026d03bf29bb230e4ede2f4417715
Rhaldney Borges <devrh3424@gmail.com>
2026-08-16 21:51:02 +0000

    refactor: improvement in the formatting of date input and for the UX interface in general

--- src/models/expense.py
+++ src/models/expense.py
@@ -14,7 +14,7 @@ class Expense:
         if isinstance(date_value, (datetime, date)):
             return date_value
         if isinstance(date_value, str):
-            for fmt in ('%Y-%m-%d', '%d/%m/%Y', 'Y-%m-%d %H:%M:%S'):
+            for fmt in ('%Y-%m-%d', '%d/%m/%Y', '%Y-%m-%d %H:%M:%S'):
                 try:
                     return datetime.strptime(date_value, fmt)
                 except ValueError:
@@ -25,28 +25,43 @@ class Expense:
     @classmethod
     def from_dict(cls,data):
         return cls(**data)
+
+    def format_field(self, field, value):
+        if field == 'amount':
+            try:
+                return locale.currency(value, grouping=True)
+            except ValueError:
+                return f'{value:.2f}'
+
+        if field == 'release_date':
+            return value.strftime('%d/%m/%Y')
+
+        return value
     
     def __str__(self):
-        try:
-            amount_formatted = locale.currency(self.amount, grouping=True)
-        except ValueError:
-            amount_formatted = f'{self.amount:.2f}'
+        amount_formatted = self.format_field(
+            'amount',
+            self.amount
+        )
 
-        date_formatted = self.release_date.strftime ('%d/%m/%Y')
+        date_formatted = self.format_field(
+            'release_date',
+            self.release_date
+        )
 
         return (
             f'Store: {self.store_name}\n'
             f'Amount: {amount_formatted}\n'
             f'Category: {self.category}\n'
             f'Payment Method: {self.payment_method}\n'
-            f'Realease Date: {date_formatted}\n'
+            f'Release Date: {date_formatted}\n'
         )
 
     def __repr__(self):
         return (
-            f'Expense(store_name={self.store_name!r}, amount={self.amount},'
-            f'category={self.category!r}, payment_method={self.payment_method!r}'
-            f'release_date={self.release_date.strftime('%Y-%m-%d')!r})'
+            f'Expense(store_name={self.store_name!r}, amount={self.amount!r},'
+            f'category={self.category!r}, payment_method={self.payment_method!r},'
+            f'release_date={self.release_date.strftime("%Y-%m-%d")!r})'
         )
 
     def to_dict(self):
--- src/services/expense_service.py
+++ src/services/expense_service.py
@@ -34,35 +34,115 @@ def list_expenses():
 
 
 def get_field_to_edit():
-    print('\nWhat do you want to edit?')
+    print('\n--- What do you want to edit? ---')
     print('1 - Store name')
     print('2 - Amount')
     print('3 - Category')
     print('4 - Payment method')
     print('5 - Release Date')
     print('6 - Everything')
+    print('0 - Cancel')
 
     while True:
         option = ui.inputs.get_int('Choice an option: ')
-        if 1 <= option <= 6:
+
+        if 0 <= option <= 6:
             return option
-        print('\nInvalid option. Please try again.\n')    
+        
+        print('\nInvalid option. Please try again.\n')
 
-def apply_expense_updates(expense, option):
-    if option in (1, 6):
-        expense.store_name = ui.inputs.get_name('Name: ')
-    if option in (2, 6):
-        expense.amount = ui.inputs.read_float('Amount: ')
-    if option in (3, 6):
-        expense.category = input('New category: ')
-    if option in (4, 6):
-        expense.payment_method = input('New payment method: ')
-    if option in (5, 6):
-        expense.release_date = ui.inputs.get_date('Release date: ')
+def get_current_value(expense, attribute):
+    value = getattr(expense, attribute, 'N/A')
+    if hasattr(expense, 'format_field'):
+        return expense.format_field(attribute, value)
+    return value
 
+def get_new_value(expense, attribute):
+    current_value = get_current_value(expense, attribute)
+
+    print(f'\nCurrent {attribute.replace("_", " ")}: {current_value}')
+
+    if attribute == 'store_name':
+        return ui.inputs.get_name('New store name: ')
+
+    if attribute == 'amount':
+        return ui.inputs.read_float('New amount: ')
+
+    if attribute == 'category':
+        return input ('New category: ').strip()
+
+    if attribute == 'payment_method':
+        return input('New payment method: ').strip()
+
+    if attribute == 'release_date':
+        return ui.inputs.get_date('New release date: ')
+
+def collect_expense_updates(expense, option):
+    fields = {
+        1: 'store_name',
+        2: 'amount',
+        3: 'category',
+        4: 'payment_method',
+        5: 'release_date'
+    }
+
+    if option == 6:
+        selected_fields = list(fields.values())
+    else:
+        selected_fields = [fields[option]]
+
+    updates = {}
+
+    for attribute in selected_fields:
+        new_value = get_new_value(expense, attribute)
+        updates[attribute] = new_value
+
+    return updates
+
+def show_changes(expense, updates):
+    print('\n--- Review Changes ---')
+
+    for attribute, new_value in updates.items():
+        old_value = getattr(expense, attribute, 'N/A')
+
+        old_display = expense.format_field(
+            attribute,
+            old_value
+        )
+
+        new_display = expense.format_field(
+            attribute,
+            new_value
+        )
+
+        field_name = attribute.replace('_', ' ').title()
+
+        print(f'\n{field_name}')
+        print(f'    Current : {old_display}')
+        print(f'    New     : {new_display}')
+
+
+    print('\n' + '-' * 40)
+
+def confirm_changes():
+    while True:
+        answer = input('\nSave these changes? (Y/N): ').strip().lower()
+
+        if answer in ('y', 'yes'):
+            return True
+
+        if answer in ('n', 'no'):
+            return False
+
+        print('Please enter Y or N')
+
+def apply_expense_updates(expense, updates):
+    for attribute, value in updates.items():
+        setattr(expense, attribute, value)
 
 def update_expense():
     expenses = read_json()
+
     if not expenses:
         print('\nNo expenses found.')
         return
@@ -70,18 +150,44 @@ def update_expense():
     list_expenses()
 
     while True:
-        index = ui.inputs.get_int('\nWhich expense do you want to edit? ')
+        index = ui.inputs.get_int(
+            '\nWhich expense do you want to edit? '
+            )
 
         if 1 <= index <= len(expenses):
             break
-        print('\nInvalid expense number.')
+
+        print('\nInvalid expense number. '
+        f'Please choose a number between 1 and {len(expenses)}.'
+        )
     
     expense = expenses[index - 1]
 
-    option = get_field_to_edit()
-    apply_expense_updates(expense, option)
+    print(f'\n---Selected Expense #{index} ---')
+    print(expense)
+    print('-' * 40)
 
+    option = get_field_to_edit()
+
+    if option == 0:
+        print('\nOperation cancelled.')
+        return
+
+    updates = collect_expense_updates(expense, option)
+
+    if not updates:
+        print('\nNo changes were made.')
+        return
+
+    show_changes(expense, updates)
+
+    if not confirm_changes():
+        print('\nOperation cancelled. No changes were saved.')
+        return
+
+    apply_expense_updates(expense, updates)
     write_json(expenses)
+
     print('Expense updated sucessfully!')
 
 def delete_expense():
@@ -97,7 +203,7 @@ def delete_expense():
         index = ui.inputs.get_int('Which expense do you want to delete? ')
 
         if not 1 <= index <= len(expenses):
-            print('\nInvalid expenser number.')
+            print('\nInvalid expense number.')
             continue
 
         expenses.pop(index - 1)
